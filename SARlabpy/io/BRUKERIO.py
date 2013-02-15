eeab402c746507bf1d9389c64a39bd247d5ca73c
Firas Moosvi <firas@moosvi.com>
2013-02-15 11:22:10 -0800

    random comment added

--- SARlabpy/io/BRUKERIO.py
+++ SARlabpy/io/BRUKERIO.py
@@ -2,6 +2,7 @@
 
 # Copyright (C) 2012-2013 Stefan A Reinsberg and SARlab members
 # full license details see LICENSE.txt
+# Random comment added by FM
 """Collection of BRUKER input routines
 
 Handy functions to read BRUKER data and header files.
