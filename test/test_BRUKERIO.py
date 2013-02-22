5dee213cd48ad38d98a241ba7f2cf4d217911d7f
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-02-21 18:56:53 -0800

    test_BRUKERIO.readfid is not ideal. It shows data.
    It is hard to come up with assertions for reading this sort of data.

--- test/test_BRUKERIO.py
+++ test/test_BRUKERIO.py
@@ -5,9 +5,12 @@ Test Routines for BRUKERIO
 import unittest
 import doctest
 import os
+import time
 
-from pylab import imshow
+import cv2
+import matplotlib.pyplot as plt
 import SARlabpy
+from SARlabpy.io import BRUKERIO
 from SARlabpy.io.BRUKERIO import readfid
 
 
@@ -16,11 +19,25 @@ class Test(unittest.TestCase):
     Unit tests for readfid.
     '''
     
-    def def_doctests(self):
+    def test_doctests(self):
         '''Run the doctests'''
-        doctests.testmod(BRUKERIO)
+        print('Running the doctests')
+        doctest.testmod(BRUKERIO)
+        
     def test_readfid(self):
         '''BRUKERIO.io.readfid()'''
-        fname = os.path.expanduser('~/datalink/Moosvi.ii2/3')
-        data = readfid(fname)
+        print('test_readfid')
+        fname = os.path.expanduser('~/datalink/NecS1Hs08.hk1/3')
+        data = readfid(fname+'/fid')
         kspace = data['data']
+        print (kspace.shape)
+        for i in range(kspace.shape[2]):
+            arr = abs(kspace[:,:,i,0])
+            cv2.imshow('a', arr / arr.max()*10)
+            cv2.waitKey(0)
+        cv2.destroyAllWindows
+            
+        self.assertEqual(kspace.shape, (128,128,3,1))
+        
+if __name__ == "__main__":
+    unittest.main()
