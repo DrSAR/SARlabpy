efe258980d8cef7ecf6750c3fc3da7471989026c
Firas Moosvi <firas@moosvi.com>
2013-02-22 16:34:17 -0800

    simplified and cleaned up data storage into dicts.

--- SARlabpy/fmoosvi/BSanalysis.py
+++ SARlabpy/fmoosvi/BSanalysis.py
@@ -5,6 +5,7 @@ Created on Tue Feb 19 11:36:19 2013
 @author: fmoosvi
 """
 
+from __future__ import division
 import SARlabpy as sar
 #import os
 import pylab as py
@@ -18,23 +19,21 @@ study_name = 'dBlochSiegert1.gP2' # could set this to be input by the system
 default_reco = '/pdata/2/'
 
 series_num = ['/36','/37','/46','/47'] # 36/37 are 0 power. 46/47 are high power
-data_dict = []
+data_dict = {}
 
-procdirname_0 = ''.join([your_path,study_name,series_num[0],default_reco])
-procdirname_1 = ''.join([your_path,study_name,series_num[1],default_reco])
-procdirname_2 = ''.join([your_path,study_name,series_num[2],default_reco])    
-procdirname_3 = ''.join([your_path,study_name,series_num[3],default_reco]) 
-  
-current_data_dict_0 = sar.read2dseq(procdirname_0,typecast=True)
-current_data_dict_1 = sar.read2dseq(procdirname_1,typecast=True)
-current_data_dict_2 = sar.read2dseq(procdirname_2,typecast=True)
-current_data_dict_3 = sar.read2dseq(procdirname_3,typecast=True)
+for i in range(0,len(series_num)):
+
+    procdirname = ''.join([your_path,study_name,series_num[i],default_reco])
+    scan_name = ''.join(['scan',str(i)])
+    current_data_dict = sar.read2dseq(procdirname,typecast=True)
+    
+    data_dict[scan_name] = current_data_dict
 
 ## Loadng in parameters as needed
     
-n_read = current_data_dict_0['header']['d3proc']['IM_SIX']
-n_phase = current_data_dict_0['header']['d3proc']['IM_SIY']
-n_slice = current_data_dict_0['header']['d3proc']['IM_SIZ']
+n_read = current_data_dict['header']['d3proc']['IM_SIX']
+n_phase = current_data_dict['header']['d3proc']['IM_SIY']
+n_slice = current_data_dict['header']['d3proc']['IM_SIZ']
 
 currslice = math.floor(n_slice/2);
  
@@ -46,15 +45,15 @@ KBS = 71.16
 
 
 # Calculate Offset and deal with phase wrap
-offset = current_data_dict_1['data'] - current_data_dict_0['data']
-offset_phase_unwrapped = numpy.unwrap(offset)
+offset = data_dict['scan0']['data'] - data_dict['scan1']['data']
+#offset_phase_unwrapped = numpy.unwrap(offset,discont=2*numpy.pi)
 
 # Calculate BS phase difference, accounting for offset, deal with phase wrap
-phase_diff = current_data_dict_3['data'] - current_data_dict_2['data'] + offset
-phase_diff_unwrapped = numpy.unwrap(phase_diff)
+phase_diff = data_dict['scan2']['data'] - data_dict['scan3']['data'] + offset
+#phase_diff_unwrapped = numpy.unwrap(phase_diff,discont=2*numpy.pi)
 
 # Calculate B1 peak
-B1peak = numpy.sqrt(numpy.absolute(phase_diff_unwrapped)//(2*KBS))
+B1peak = numpy.sqrt(numpy.absolute(phase_diff)//(2*KBS))
 
 # Calculate the flip angle for the pulse of interest 
 gamma = 267.513e6
