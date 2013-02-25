56eeb2a2229d6b038dae9f8b749bace37a85a76e
Firas Moosvi <firas@moosvi.com>
2013-02-25 11:23:49 -0800

    testing source tree.

--- SARlabpy/fmoosvi/analysis.py
+++ SARlabpy/fmoosvi/analysis.py
@@ -1,6 +1,6 @@
 # Copyright (C) 2012-2013 Stefan A Reinsberg and SARlab members
 # full license details see LICENSE.txt
-# you suck.
+# you stuck.
 """Collection of analysis routines for Bruker data
 
 
