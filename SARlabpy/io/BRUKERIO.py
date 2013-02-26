336606112078a8902a1ce7df98bfa5484bb8e8be
Firas Moosvi <firas@moosvi.com>
2013-02-26 12:14:57 -0800

    adjusted read2dseq to take in scandirname instead of procdirname
    fixed issue 15
    
    https://github.com/DrSAR/SARlabpy/issues/15

--- SARlabpy/io/BRUKERIO.py
+++ SARlabpy/io/BRUKERIO.py
@@ -592,8 +592,7 @@ def read2dseq(procdirname):
     :return: dictionary with data, and headerinformation. The data is
              an array of BRUKER-reconstructed image data in the respecive proc
              directory.
-    :rtype: dict with 'data':
-            numpy array 'header':dict{'reco':..., 'd3proc':...}
+    :rtype: dict with 'data':numpy array 'header':dict{'recpo':..., 'd3proc':...}
     :raises: IOERROR if directory non-existent
 
     This relies on numpy's array functionality
@@ -607,10 +606,14 @@ def read2dseq(procdirname):
     ## Enhancement by Firas M. to also read in acqp and mehod files
     # Currently non-pythonic because it requres inefficient code
     
-    scandirname = os.path.dirname(os.path.dirname(procdirname))
+    scandirname = os.path.split(procdirname)
+    scandirname = os.path.split(scandirname[0])
+    scandirname = os.path.split(scandirname[0])
     
-#    acqp = readJCAMP(scandirname[0]+'/acqp')
-    method = readJCAMP(os.path.join(scandirname,'method'))
+    acqp = readJCAMP(scandirname[0]+'/acqp')
+    method = readJCAMP(scandirname[0]+'/method')
+    
+    
     
     RECO_size = [d3proc['IM_SIZ'], \
                  d3proc['IM_SIY'], \
@@ -650,20 +653,19 @@ def read2dseq(procdirname):
     assert len(method['PVM_SPackArrNSlices']) == 1, 'Something wrong in num_slices'
     reps = method['PVM_NRepetitions']
     
-#    new_data = numpy.empty([d3proc['IM_SIZ']/reps, d3proc['IM_SIY'],d3proc['IM_SIX'],reps])
-#
-#    if num_slices> 1: # have a 4D case:
-#        
-#        for slice in range(0,num_slices-1):
-#        
-#            new_data[slice,:,:,:] = data[slice::num_slices,:,:].transpose(1,2,0)
-#            new_data[slice,:,:,:] = data[slice::num_slices,:,:].transpose(1,2,0)
-#        
-#        data = new_data
+    new_data = numpy.empty([d3proc['IM_SIZ']/reps, d3proc['IM_SIY'],d3proc['IM_SIX'],reps])
+
+    if num_slices> 1: # have a 4D case:
+        
+        for slice in range(0,num_slices-1):
+        
+            new_data[slice,:,:,:] = data[slice::num_slices,:,:].transpose(1,2,0)
+        
+        data = new_data
                                   
     return {'data':data,
             'isImage':True,
-            'header':{'reco': reco, 'd3proc':d3proc}}
+            'header':{'reco': reco, 'd3proc':d3proc, 'acqp': acqp, 'method': method}}
 
 def dict2string(d):
     '''
@@ -767,7 +769,7 @@ def readRFshape(filename):
 
 # main part - run test cases if called as a module
 if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
+#    import doctest
+#    doctest.testmod()
     fn = os.path.expanduser('~/data/readfidTest.ix1/1/acqp')
     hdr=readJCAMP(fn)
