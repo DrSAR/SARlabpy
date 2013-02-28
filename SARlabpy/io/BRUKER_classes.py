287af67967e78ae227018370183c5ef3b6a046d5
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-02-28 02:11:36 -0800

    fixes #20 fixes 21 : thin class candy over all the 2dseq, fid, and JCAMP readers
    read2dseq seems buggy -> committing some changes to make it run
    bugfix still required (see issue #22)

--- SARlabpy/io/BRUKER_classes.py
+++ SARlabpy/io/BRUKER_classes.py
@@ -4,17 +4,45 @@ Class Definitions for BRUKER data
 """
 import os
 import glob
-import SARlabpy.io.BRUKERIO
+import BRUKERIO
 
 import logging
 logger=logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
+#TODO: Class for a Patient
+class Patient(object):
+    def __init__(self):
+        raise NotImplementedError
+        
+#TODO: Class for a Study
+class Study(object):
+    def __init__(self):
+        raise NotImplementedError
+
 class Scan(object):
     '''
     Object to represent a BRUKER scan
 
-    The __init__() method expects the filename to be a directory
+    The __init__() method expects the filename to be a directory. It will
+    try around a bit (filenames inside the directory) before throwing an 
+    exception. It will also give up if it can't find any of the
+    acqp, method, fid or one 2dseq file.
+    
+    Simple Example:
+        
+        >>> import os
+        >>> fname = os.path.expanduser('~/data/readfidTest.ix1/3')
+        >>> exp3 = Scan(fname)
+        >>> dir(exp3)
+        ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'acqp', 'fid', 'method', 'pdata']
+        >>> exp3.acqp.ACQ_protocol_name
+        'FLASH_bas(modified)'
+        >>> exp3.acqp.ORIGIN
+        'Bruker BioSpin MRI GmbH'
+        >>> exp3.acqp.DATE
+        'Thu Feb 21 19:01:26 2013 PST (UT-8h)  '
+
     '''
     def __init__(self, filename):
         '''
@@ -54,9 +82,9 @@ class Scan(object):
         
         # let's see whether any data has been processed
         self.pdata=[]
-        for f in glob.glob(os.path.join(dirname,'pdata','*','2dseq')):
+        for f in glob.glob(os.path.join(dirname,'pdata','*')):
             try:
-                self.pdata.append(PDATA_2dseq_file(f))
+                self.pdata.append(PDATA_file(f))
             except IOError:
                 pass
 
@@ -67,26 +95,72 @@ class Scan(object):
                 'files').format(dirname))
                             
 class JCAMP_file(object):
+    '''
+    Represents a JCAMP encoded parameter file.
+    
+    Parameters become attributes in this class
+    '''
     def __init__(self, filename):
         if not os.path.isfile(filename):
             raise IOError('File "%s" not found' % filename)
         # no go and read the file
-        acqp = SARlabpy.io.BRUKERIO.readJCAMP(filename)
+        acqp = BRUKERIO.readJCAMP(filename)
         for k,v in acqp.iteritems():
-            self.__dict__[k]=v
+            self.__dict__[k] = v
 
 class ACQP_file(JCAMP_file):
+    '''
+    Thin specialization of JCAMP_file. Mostly just a place holder and
+    a place to store the fact that these files are called "acqp".
+    '''
     def __init__(self, dirname):
         super(ACQP_file, self).__init__(os.path.join(dirname,'acqp'))
 
 class METHOD_file(JCAMP_file):
+    '''
+    Thin specialization of JCAMP_file. Mostly just a place holder and
+    a place to store the fact that these files are called "method".
+    '''
     def __init__(self, dirname):
         super(METHOD_file, self).__init__(os.path.join(dirname,'method'))
         
 class BRUKER_fid(object):
-    def __init__(self, dirname):
-        raise IOError('not yet implemented')
+    '''
+    Initialize an fid object whih should sit in the scan root directory.
+    
+    __init__(filename) expects a directory name. It will look
+    for the fid file, and the acqp and method parameter files.
+    '''
+    def __init__(self, dirname, lazy=False):
+        if lazy:
+            raise NotImplementedError('lazy loading not implement yet')
+        else:
+            fid = BRUKERIO.readfid(os.path.join(dirname,'fid'))
+            self.__dict__['fid'] = fid['data']
 
-class PDATA_2dseq_file(object):
+
+class dict2obj(object):
+    '''
+    Helper class to turn dictionary keys into class attributes.
+    It's so much nicer to refer to them that way.
+    '''
+    def __init__(self,dictionary):
+        for k,v in dictionary.iteritems():
+            self.__dict__[k] = v
+
+class PDATA_file(object):
+    '''
+    Initialize a processed data set that usually sits in */pdata/[1-9].
+
+    __init__(filename) expects a directory name in pdata. It will look
+    for the 2dseq file, the d3proc and th reco file.
+    '''
     def __init__(self, filename):
-        raise IOError('not yet implemented')
+        pdata = BRUKERIO.read2dseq(filename)
+        self.__dict__['data'] = pdata['data']
+        for k,v in pdata['header'].iteritems():
+            self.__dict__[k] = dict2obj(v)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
