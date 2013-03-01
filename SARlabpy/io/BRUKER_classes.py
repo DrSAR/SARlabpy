8233e0b9110f2bbcbd5e31a9777e3cc3cde24e33
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-02-28 16:00:25 -0800

    fixed #19: lazy loading and decorator access now enabled for all
    TODO: find good heuristics for when to fail
    (currently quite permissive)
    Avoid double loading of acqp, method files.
    (this might require deep changes in readfid)
    document & test

--- SARlabpy/io/BRUKER_classes.py
+++ SARlabpy/io/BRUKER_classes.py
@@ -10,6 +10,130 @@ import logging
 logger=logging.getLogger(__name__)
 logger.addHandler(logging.NullHandler())
 
+                            
+class JCAMP_file(object):
+    '''
+    Represents a JCAMP encoded parameter file.
+    
+    Parameters become attributes in this class
+    '''
+    def __init__(self, filename, lazy=True):
+        if not os.path.isfile(filename):
+            raise IOError('File "%s" not found' % filename)
+        self.filename = filename
+        self.__yet_loaded = False
+        if not lazy:
+            self.__forced_load()
+            
+    def __getattr__(self, attr):
+        '''
+        intercepts only undefined attributes. this will be used to 
+        trigger the loading of the data
+        '''
+        if not self.__yet_loaded:
+            self.__forced_load() 
+        return object.__getattribute__(self, attr)
+        
+    def __forced_load(self):
+        logger.info('loading %s now forced' % self.filename)        
+        acqp = BRUKERIO.readJCAMP(self.filename)
+        for k,v in acqp.iteritems():
+            self.__dict__[k] = v
+        self.__yet_loaded = True
+        
+class ACQP_file(JCAMP_file):
+    '''
+    Thin specialization of JCAMP_file. Mostly just a place holder and
+    a place to store the fact that these files are called "acqp".
+    '''
+    def __init__(self, dirname, lazy=True):
+        super(ACQP_file, self).__init__(os.path.join(dirname,'acqp'), 
+                                        lazy=lazy)
+
+class METHOD_file(JCAMP_file):
+    '''
+    Thin specialization of JCAMP_file. Mostly just a place holder and
+    a place to store the fact that these files are called "method".
+    '''
+    def __init__(self, dirname, lazy=True):
+        super(METHOD_file, self).__init__(os.path.join(dirname,'method'),
+                                          lazy=lazy)
+        
+class BRUKER_fid(object):
+    '''
+    Initialize an fid object whih should sit in the scan root directory.
+    Really, this is a decorator class.
+    
+    __init__(filename) expects a directory name. It will look
+    for the fid file, and the acqp and method parameter files.
+    '''
+    def __init__(self):
+        self.__yet_loaded = False
+            
+#    def __getattr__(self, attr):
+#        '''
+#        intercepts only undefined attributes. this will be used to 
+#        trigger the loading of the fid data
+#        '''
+#        if not self._yet_loaded:
+#            self._forced_load() 
+#        return object.__getattribute__(self, attr)
+    
+    def __set__(*args): raise AttributeError
+        
+    def __get__(self, instance, value):
+        if not self.__yet_loaded:
+            self.__forced_load(instance.dirname) 
+        return self.fid
+        
+    # TODO: currently, header files acqp and method are loaded twice.
+    #       need to remove the redundancy. Somehow provide the headers
+    #       if possible.
+    def __forced_load(self, filename):
+        logger.info('delayed loading of "%s" now forced ...' % filename)
+        fid = BRUKERIO.readfid(os.path.join(filename,'fid'))
+        self.fid = fid['data']
+        self.__yet_loaded = True
+        
+
+class dict2obj(object):
+    '''
+    Helper class to turn dictionary keys into class attributes.
+    It's so much nicer to refer to them that way.
+    '''
+    def __init__(self,dictionary):
+        for k,v in dictionary.iteritems():
+            self.__dict__[k] = v
+
+class PDATA_file(object):
+    '''
+    Initialize a processed data set that usually sits in */pdata/[1-9].
+
+    __init__(filename) expects a directory name in pdata. It will look
+    for the 2dseq file, the d3proc and th reco file.
+    '''
+    def __init__(self, filename, lazy=True):
+        self.__yet_loaded = False
+        if not lazy:
+            self.__forced_load()
+    def  __forced_load(self):
+        logger.info('loading 2dseq now forced %s:' % self.filename)
+        pdata = BRUKERIO.read2dseq(self.filename)
+        self.__yet_loaded = True
+        self.__dict__['data'] = pdata['data']
+        for k,v in pdata['header'].iteritems():
+            self.__dict__[k] = dict2obj(v)
+
+    def __getattr__(self, attr):
+        '''
+        intercepts only undefined attributes. this will be used to 
+        trigger the loading of the data
+        '''
+        if not self.__yet_loaded:
+            self.__forced_load() 
+        return object.__getattribute__(self, attr)
+
+
 #TODO: Class for a Patient
 class Patient(object):
     def __init__(self):
@@ -35,7 +159,7 @@ class Scan(object):
         >>> fname = os.path.expanduser('~/data/readfidTest.ix1/3')
         >>> exp3 = Scan(fname)
         >>> dir(exp3)
-        ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'acqp', 'fid', 'method', 'pdata']
+        ['__class__', '__delattr__', '__dict__', '__doc__', '__format__', '__getattribute__', '__hash__', '__init__', '__module__', '__new__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', 'acqp', 'dirname', 'fid', 'method', 'pdata']
         >>> exp3.acqp.ACQ_protocol_name
         'FLASH_bas(modified)'
         >>> exp3.acqp.ORIGIN
@@ -44,7 +168,8 @@ class Scan(object):
         'Thu Feb 21 19:01:26 2013 PST (UT-8h)  '
 
     '''
-    def __init__(self, filename):
+    fid = BRUKER_fid()
+    def __init__(self, filename, lazy=True):
         '''
         Is the filename a direcotry and can we at least find
         one of acqp, fid, or 2dseq
@@ -53,13 +178,13 @@ class Scan(object):
         '''
         if os.path.isdir(filename):
             # good, that could be it
-            dirname = filename
+            self.dirname = filename
         elif os.path.isdir(os.path.dirname(filename)):
             logger.warning(('Scan initialized with a filename (%s) not a' +
                             ' directory.') % filename)
             # did the punter point the filename to some file inside
             # the directory?
-            dirname = os.path.dirname(filename)
+            self.dirname = os.path.dirname(filename)
         else:
             # no good
             raise IOError(
@@ -68,137 +193,42 @@ class Scan(object):
 
         # see whether we can find a useful BRUKER file
         try:
-            self.acqp = ACQP_file(dirname)
+            self.acqp = ACQP_file(self.dirname, lazy=lazy)
         except IOError:
             self.acqp = None
         try:
-            self.fid = BRUKER_fid(dirname)
-        except IOError:
-            self.fid = None
-        try:
-            self.method = METHOD_file(dirname)
+            self.method = METHOD_file(self.dirname, lazy=lazy)
         except IOError:
             self.method = None
         
         # let's see whether any data has been processed
         self.pdata=[]
-        for f in glob.glob(os.path.join(dirname,'pdata','*')):
+        for f in glob.glob(os.path.join(self.dirname,'pdata','*')):
             try:
                 self.pdata.append(PDATA_file(f))
             except IOError:
                 pass
 
-        if not any([self.acqp, self.fid, self.method]):
-            #we have not found anything
+        # for success we need at least an image file.
+        # or some acqp/method files as a miminum
+        if (all([not self.acqp, not self.method, not self.pdata])):
             raise IOError(
                 ('Directory "{0}" did not contain any of the typical BRUKER '+
-                'files').format(dirname))
-                            
-class JCAMP_file(object):
-    '''
-    Represents a JCAMP encoded parameter file.
-    
-    Parameters become attributes in this class
-    '''
-    def __init__(self, filename, lazy=True):
-        if not os.path.isfile(filename):
-            raise IOError('File "%s" not found' % filename)
-        self.filename = filename
-        self._yet_loaded = False
-        if not lazy:
-            self._forced_load()
-            
-    def __getattr__(self, attr):
-        '''
-        intercepts only undefined attributes. this will be used to 
-        trigger the loading of the data
-        '''
-        if not self._yet_loaded:
-            self._forced_load() 
-        return object.__getattribute__(self, attr)
-        
-    def _forced_load(self):
-        logger.info('loading %s' % self.filename)
-        acqp = BRUKERIO.readJCAMP(self.filename)
-        for k,v in acqp.iteritems():
-            self.__dict__[k] = v
-        self._yet_loaded = True
-        
-class ACQP_file(JCAMP_file):
-    '''
-    Thin specialization of JCAMP_file. Mostly just a place holder and
-    a place to store the fact that these files are called "acqp".
-    '''
-    def __init__(self, dirname):
-        super(ACQP_file, self).__init__(os.path.join(dirname,'acqp'))
+                'files').format(self.dirname))
+                
+        # this feels kludgy but this will cause AttributeErrors when the 
+        # user tries to access objects that don't exist (rather than getting
+        # a None)
+        for attr in ['acqp', 'method','pdata']:
+            if not self.__dict__[attr]:
+                self.__delattr__(attr)
 
-class METHOD_file(JCAMP_file):
-    '''
-    Thin specialization of JCAMP_file. Mostly just a place holder and
-    a place to store the fact that these files are called "method".
-    '''
-    def __init__(self, dirname):
-        super(METHOD_file, self).__init__(os.path.join(dirname,'method'))
-        
-class BRUKER_fid(object):
-    '''
-    Initialize an fid object whih should sit in the scan root directory.
-    
-    __init__(filename) expects a directory name. It will look
-    for the fid file, and the acqp and method parameter files.
-    '''
-    def __init__(self, dirname, lazy=True):
-        self.dirname = dirname
-        self._yet_loaded = False
-        if not lazy:
-            self._forced_load()
-            
-    def __getattr__(self, attr):
-        '''
-        intercepts only undefined attributes. this will be used to 
-        trigger the loading of the fid data
-        '''
-        if not self._yet_loaded:
-            self._forced_load() 
-        return object.__getattribute__(self, attr)
-        
-    # TODO: currently, header files acqp and method are loaded twice.
-    #       need to remove the redundancy. Somehow provide the headers
-    #       if possible.
-    def _forced_load(self):
-        logger.info('loading %s' % self.dirname)
-        fid = BRUKERIO.readfid(os.path.join(self.dirname,'fid'))
-        self.__dict__['fid'] = fid['data']
-        self._yet_loaded = True
-        
-
-class dict2obj(object):
-    '''
-    Helper class to turn dictionary keys into class attributes.
-    It's so much nicer to refer to them that way.
-    '''
-    def __init__(self,dictionary):
-        for k,v in dictionary.iteritems():
-            self.__dict__[k] = v
-
-class PDATA_file(object):
-    '''
-    Initialize a processed data set that usually sits in */pdata/[1-9].
-
-    __init__(filename) expects a directory name in pdata. It will look
-    for the 2dseq file, the d3proc and th reco file.
-    '''
-    def __init__(self, filename):
-        pdata = BRUKERIO.read2dseq(filename)
-        self.__dict__['data'] = pdata['data']
-        for k,v in pdata['header'].iteritems():
-            self.__dict__[k] = dict2obj(v)
 
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
-#    import numpy
-#    fname = os.path.expanduser('~/data/readfidTest.ix1/3')
-#    x = BRUKER_fid(fname)
-#    print('mean = {0}'.format(numpy.mean(x.fid)))
-#    print(x.kasimir)
+    import numpy
+    fname = os.path.expanduser('~/data/readfidTest.ix1/3')
+    x = Scan(fname)
+    print('mean = {0}'.format(numpy.mean(x.fid)))
+    print(x.kasimir)
