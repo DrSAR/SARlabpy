34b397f5df3d4fa86c8f1c7f95a5f67cee744f83
Firas Moosvi <firas@moosvi.com>
2013-03-12 14:01:21 -0700

    minor modifications

--- SARlabpy/fmoosvi/analysis.py
+++ SARlabpy/fmoosvi/analysis.py
@@ -22,7 +22,7 @@ def describe_object(class_object):
         for study in class_object.studies:
             print('-'*40+'\n'+study.subject.SUBJECT_id)    
     except: 
-        try: # if multile studies, print study names and scans         
+        try: # if multiple studies, print study names and scans         
             for study in class_object:
                 print('-'*40+'\n'+study.subject.SUBJECT_id)    
                 for scan in study.scans:
@@ -33,6 +33,40 @@ def describe_object(class_object):
                     print("  "+scan.acqp.ACQ_protocol_name)      
             except: #otherwise print useless error message
                 print("I don't know what's going on")
+
+def calculate_AUC(scan_object, time = 60, pdata = 1):
+    
+    # Define and extract necessary parameters
+    x_size = scan_object.visu_pars.VisuCoreSize[0]
+    y_size = scan_object.visu_pars.VisuCoreSize[1]
+    scan_object.visu_pars.VisuAcqPhaseEncSteps
+    
+    
+    # Read in data and headers    
+    x_size = data_dict['data'].shape[0]
+    y_size = data_dict['data'].shape[1]    
+    num_slices = data_dict['header']['method']['PVM_SPackArrNSlices'][0]
+    repetition_time = data_dict['header']['method']['PVM_RepetitionTime']*1E-3
+    phase_encodes = data_dict['header']['method']['PVM_EncMatrix'][1] # Num phase encoding steps
+    reps = data_dict['header']['method']['PVM_NRepetitions']
+    time_per_rep = repetition_time * phase_encodes
+    auc_reps = int(numpy.round(time / time_per_rep))
+    time_points = numpy.arange(time_per_rep,time_per_rep*auc_reps + time_per_rep,time_per_rep)
+    
+    # Determine point of injection by averaging one slice in the entire image
+    inj_point = sar.analysis.inj_point(data_dict)
+    
+    # Now calculate the Normalized Intesity voxel by voxel
+    norm_data = normalize_dce(data_dict)
+
+    # Now calculate the actual AUC
+    auc_data = numpy.empty([x_size,y_size,num_slices])
+    
+    for slice in range(num_slices):
+        auc_data[:,:,slice] = scipy.integrate.simps(norm_data[:,:,slice,inj_point:inj_point+auc_reps],x=time_points)
+    return auc_data
+
+
 
 
 def calculate_AUC(data_dict, time = 60):
