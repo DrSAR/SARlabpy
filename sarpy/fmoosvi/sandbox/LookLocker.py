570ce5adaa62e26bbcc43eef780855d2ad11a6b7
Firas Moosvi <firas@moosvi.com>
2013-03-14 14:14:40 -0700

    Cleaning up the sandbox

--- sarpy/fmoosvi/sandbox/LookLocker.py
+++ sarpy/fmoosvi/sandbox/LookLocker.py
@@ -6,3 +6,13 @@ Created on Wed Mar 13 22:41:46 2013
 """
 
 ## Porting over LookLocker code from Annie
+
+import sarpy
+import sarpy.fmoosvi.analysis
+import sarpy.fmoosvi.getters
+import sarpy.fmoosvi.wrappers
+
+
+NecS3 = sarpy.Experiment('NecS3')
+
+LLdata = NecS3.find_scan_by_protocol('04_ubcLL2')
