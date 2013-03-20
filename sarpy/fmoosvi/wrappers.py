abf8b3ff1f36091a1e04bd4ae3376926f18650c0
Firas Moosvi <firas@moosvi.com>
2013-03-19 19:01:26 -0700

    Lots of good stuff!
    - LL fitting now works
    - BSB1 map now works!
    - added wrapper to calculate T1 map using LL
    - removed BlochSiegert.py from sandbox
    - removed fitCode.py from sandbox
    - Added compare_B1_maps.py to compare B1 maps from BSB1flash, BSB1msme, VFA

--- sarpy/fmoosvi/wrappers.py
+++ sarpy/fmoosvi/wrappers.py
@@ -5,46 +5,97 @@ Created on Wed Mar 13 14:11:03 2013
 @author: fmoosvi
 """
 
+import numpy
 import sarpy
 import sarpy.fmoosvi.analysis
 
-def calculate_AUC(Bruker_object, protocol_name = '06_FLASH2D.6sl-DCE', pdata_num = 0, time = 60):
-    
-    scan_list = []
-    auc = []
-    
-    if type(Bruker_object) == sarpy.io.BRUKER_classes.Experiment:
-        scan_list = Bruker_object.find_scan_in_experiment(protocol_name)
-        print('You put in an experiment')
-        
-    elif type(Bruker_object) == sarpy.io.BRUKER_classes.Study:
-        scan_list = Bruker_object.find_scan(protocol_name)
-        print('You put in a single study')
-            
-    elif type(Bruker_object) == sarpy.io.BRUKER_classes.Scan:
-        scan_list = Bruker_object
-        print('You put in a single scan')
-            
-    elif type(Bruker_object) == list:
-        
-        if type(Bruker_object[0]) == sarpy.io.BRUKER_classes.Study:                                  
+def calculate_AUC(Bruker_object, protocol_name = '06_FLASH2D+', pdata_num = 0, time = 60):
+       
+    try:
+        scan_list = Bruker_object.find_scan_by_protocol(protocol_name)
+    except:
+        try: 
+            scan_list = []                                
             for study in Bruker_object:
                 scan_list.append(study.find_scan(protocol_name))
-            print('You put in a list of studies')
+            print('You put in a list of studies, try to avoid that')
+        except:
+            raise
 
-        elif type(Bruker_object[0]) == sarpy.io.BRUKER_classes.Scan:            
-             scan_list = Bruker_object
-             print('You put in what you were supposed to!')
-            
+    ## Now to calculate the AUC
+
+    auc = []
+
     for scan in scan_list:
         auc.append(sarpy.fmoosvi.analysis.h_calculate_AUC(scan))
         
     return auc
     
 def calculate_BSB1map(Experiment_object, protocol_name = '04_ubcLL2'):
+
+#TODO: Implemen BSB1 mapwrapper around h_BS_B1map
+
+######################### Start Example #########################
+## This is for one phantom study using LL and BSB1 MSME
+#APExp = sarpy.Experiment('DiLL.iI1')
+#BS_scans = APExp.find_scan_by_protocol('09\-2DBSB1map\-MSME_bas\(modified\)')[-4:]
+#
+#LLdata = sarpy.Experiment('DiLL.iI1').find_scan_by_protocol('MOBILE*')[0]
+#
+#zero_BS_minus = BS_scans[0]
+#zero_BS_plus = BS_scans[1]
+#power_BS_minus = BS_scans[2]
+#power_BS_plus = BS_scans[3]
+#
+#b1map = sarpy.fmoosvi.analysis.h_BS_B1map(zero_BS_minus,\
+#                                          zero_BS_plus,\
+#                                          power_BS_minus,\
+#                                          power_BS_plus,\
+#                                          LLdata)
+#                                          
+#pylab.imshow(b1map[:,:,0,0], aspect = 0.5)
+#pylab.colorbar()
+######################### End Example #########################
+
+    return #b1map
+    
+def calculate_T1map(Bruker_object, protocol_name = 'MOBILE+', flip_angle_map = 0):
+
+
+#TODO: Fix this so the list/object situation is sorted!
+#    try:
+#        
+#        if type(Bruker_object) == sarpy.io.BRUKER_classes.Scan:
+#            scan_list = Bruker_object
+#        else:
+#            scan_list = Bruker_object.find_scan_by_protocol(protocol_name)
+#    except:
+#        try: 
+#            scan_list = []                                
+#            for study in Bruker_object:
+#                scan_list.append(study.find_scan_by_protocol(protocol_name))
+#            print('You put in a list of studies, try to avoid that')
+#        except:
+#            raise
+
+    ## Now to calculate the AUC
+    
+    scan_list = Bruker_object
+    T1_map = []
+
+    for scan in scan_list:
+        T1_map.append(sarpy.fmoosvi.analysis.h_fit_T1_LL(Bruker_object,\
+                                                      flip_angle_map = numpy.fliplr(flip_angle_map)))
+    return T1_map
     
     
     
     
-    return
+    
+    
+    
+    
+    
+    
+    
     
