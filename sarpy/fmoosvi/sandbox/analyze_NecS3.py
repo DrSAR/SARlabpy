9f4056a0f3183a5072bfd13a49aca62cf69115d3
Firas Moosvi <firas@moosvi.com>
2013-03-21 17:17:12 -0700

    Generating T1 Maps for NecS3

--- sarpy/fmoosvi/sandbox/analyze_NecS3.py
+++ sarpy/fmoosvi/sandbox/analyze_NecS3.py
@@ -8,7 +8,7 @@ import sarpy
 import numpy
 from sarpy.fmoosvi import wrappers
 import time
-
+import pylab
 
 NecS3_exp = sarpy.Experiment('NecS3')
 NecS3_LLscans = NecS3_exp.find_scan_by_protocol('04_ubcLL+')
@@ -36,4 +36,10 @@ print 'This run took {0} seconds.'.format(round(end_time - start_time))
 #    scan.store_adata(key='BSB1map_FLASH', data = BSB1map_FLASH)
 #    scan.store_adata(key='T1map_LL_MSME', data = T1map_LL_MSME)
 #    scan.store_adata(key='T1map_LL_FLASH', data= T1map_LL_FLASH)        
-    
+
+
+sample = NecS3_LLscans[0].adata['T1map_LL'].data.get_data()
+fig_sample = pylab.imshow(sample[0,:,:,2])
+fig_sample.set_clim(150,2500)
+pylab.colorbar()
+pylab.show()
