b975cc9755d6481cde5a7bf86074342fd142b577
Firas Moosvi <firas@moosvi.com>
2013-03-25 13:35:13 -0700

    generates figures for each animal, mostly working.

--- sarpy/fmoosvi/sandbox/summaries/summaries.py
+++ sarpy/fmoosvi/sandbox/summaries/summaries.py
@@ -12,15 +12,32 @@ for patient in NecS3_patients:
 
     LL = patient.find_scan_by_protocol('04')
     
-        fig = pylab.figure()
+    fig = pylab.figure()
 
     for session in LL:
         try:
+
+            data1 = LL[0].adata['T1map_LL'].data.get_data()
+            data2 = LL[1].adata['T1map_LL'].data.get_data()
             
-            ax1 = fig.add_subplt(1,2,1)
-            ax1.imshow(LL[0].adata['T1map_LL'].data.get_data()[:,:,0])
+            ax1 = fig.add_subplot(1,2,1)
+            a = pylab.imshow(data1[0,:,:,0])
+            a.set_clim(600,3200)
+            pylab.colorbar()
+            fig.show()
             
-            ax2 = fig.add_subplt(1,2,2)
-            ax1.imshow(LL[1].adata['T1map_LL'].data.get_data()[:,:,1])
+            ax2 = fig.add_subplot(1,2,2)
+            b = pylab.imshow(data2[0,:,:,1])
+            b.set_clim(600,3200)
+            pylab.colorbar()
+            fig.show()
+
         except:
-           # print('You sir, are unhelpful')
+            print('You sir, are unhelpful')
+            
+#            
+#pylab.figure()
+#img = pylab.imshow(proc_data[0][:,:,0], aspect = 0.5)
+#img.set_clim(0.0,1200)
+#pylab.colorbar()
+#pylab.show()
