6cc4d56cdb2d8d99186ff633bf0b7216e43c6590
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-03-26 17:22:54 -0700

    we can now calculate the rotation matrix from visu_pars
    assertions included to check for approximately correct
    values as compared to dcm2nii

--- sarpy/io/check_nii_vs_visu_pars.py
+++ sarpy/io/check_nii_vs_visu_pars.py
@@ -55,13 +55,19 @@ A case of multi-slice 2D
 
 '''
 
-def load_all_nii(filename=os.path.expanduser('~'),
+def nii_geom(filename=os.path.expanduser('~'),
                  visu_dict=None):
     v = visu_dict
-    a = nibabel.Nifti1Image.from_filename(filename)
-    print a.get_affine()[0:3, 0:3].reshape(9).round(3)
-    M_inv = numpy.matrix(v.VisuCoreOrientation[0]).reshape(3,3).I
-    print M_inv.reshape(9).round(3)
+    nii_img = nibabel.Nifti1Image.from_filename(filename)
+    R_nii = nii_img.get_affine()[0:3, 0:3].reshape(9)
+    pixdims = nii_img.get_header()['pixdim']
+    print('affine:\n{0}'.format(R_nii.round(3)))
+    
+    M = numpy.matrix(v.VisuCoreOrientation[0]).reshape(3,3)
+    M_inv = M.I
+    LPS_2_RAS = numpy.array([-1,1,-1, -1,1,-1, 1,-1,1])
+    M_inv_RAS = LPS_2_RAS * numpy.array(M_inv.reshape(9))
+
     pixdims = numpy.array(v.VisuCoreExtent).astype('float')/ \
               numpy.array(v.VisuCoreSize)
     # for 2D we still need to figure out the 3rd dimension
@@ -77,11 +83,34 @@ def load_all_nii(filename=os.path.expanduser('~'),
 
     if len(pixdims) != 3:
         raise ValueError('unexpected value for VisuCoreDim')
+        
+    fktr = numpy.tile(pixdims,3)
+    R_visupars = M_inv_RAS.reshape(9) * fktr
+    print('visupars:\n{0}'.format(R_visupars.round(3)))
+    print('-'*80)
     
-    print pixdims
+    if not numpy.allclose(R_nii, R_visupars, atol=1e-4, rtol=1e-4):
+        raise AssertionError('Rotation Matrices differ')
+
+    # quaternion approach
+#    b = nii_img.get_header()['quatern_b']
+#    c = nii_img.get_header()['quatern_c']
+#    d = nii_img.get_header()['quatern_d']
+#    a=numpy.sqrt(1-b**2-c**2-d**2)
+#    quatern_mat = nibabel.quaternions.quat2mat([a, b,c,d])
+#    M_inv_RAS = LPS_2_RAS * numpy.array(M_inv.reshape(9))
+#    print quatern_mat.round(5).reshape(9)
+#    print M_inv_RAS.round(5)[0]
+#    if numpy.allclose(quatern_mat.reshape(9),  M_inv_RAS[0],
+#                      rtol=1e-3, atol=1e-3):
+#        print ('close!')
+#    print('nifti-quat = [{0} {1} {2} {3}]'.format(a,b,c,d))
+#    print(nibabel.quaternions.mat2quat(M_inv_RAS))
     
-#        fktr = numpy.tile(pixdims.reshape(3,1),3).reshape(9)
-#        print (numpy.asarray(M_inv.reshape(9)) * fktr).round(3)
+    
+    shift = nii_img.get_affine()[0:3,3]
+    print(shift.round(3))
+    print(v.VisuCorePosition[0,:] * [-1, 1, 1])
 
 if __name__ == '__main__':
 
@@ -98,7 +127,7 @@ if __name__ == '__main__':
                 if os.path.isfile(nii_file):
                     print('\n'+nii_file)
                     print('{0}\ ({1})'.format(scan.acqp.ACQ_protocol_name,                                    ' '.join(scan.method.PVM_SPackArrSliceOrient)))
-                    load_all_nii(nii_file, pdata.visu_pars)
+                    nii_geom(nii_file, pdata.visu_pars)
                                 
 #    for filename in nii_list:
 #        uid = re.sub('.*\/','',filename)
