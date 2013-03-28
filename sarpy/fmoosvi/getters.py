af30142f2ad21f32dab59fe537175e982b871b44
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-03-27 19:37:41 -0700

    automated master_sheet

--- sarpy/fmoosvi/getters.py
+++ sarpy/fmoosvi/getters.py
@@ -6,6 +6,8 @@ Created on Tue Mar 12 14:41:54 2013
 """
 
 import sarpy
+import json
+import collections
 
 def get_num_slices(scan_object, pdata_num = 0):
     
@@ -39,7 +41,7 @@ def get_num_slices(scan_object, pdata_num = 0):
     return num_slices
 
 
-def get_patients_from_experiment(Experiment_name, check = False):
+def get_patients_from_experiment(Experiment_name, verbose = False):
     
     subject_list = get_unique_list_elements(sarpy.Experiment(Experiment_name).get_SUBJECT_id())
 
@@ -49,7 +51,7 @@ def get_patients_from_experiment(Experiment_name, check = False):
         curr_patient = sarpy.Patient(subject)
         Patients.append(curr_patient)        
         
-        if check:
+        if verbose:
             print('Patient {0} has {1} sessions.'.format(subject,len(curr_patient.studies)))
     
     return Patients
@@ -69,4 +71,94 @@ def get_unique_list_elements(list, idfun=None):
        if marker in seen: continue
        seen[marker] = 1
        result.append(item)
-   return result            
+   return result         
+
+def magic_tuple(scn):
+    return (scn.shortdirname, scn.acqp.ACQ_protocol_name)
+
+if __name__ == '__main__':
+#    pat_list = get_patients_from_experiment('NecS3', verbose = True)
+#    
+#    master_sheet = collections.OrderedDict()
+#    for pat in pat_list:
+#        nm = pat.get_SUBJECT_id()
+#        assert len(set(nm)) ==1
+#        master_sheet[nm[0]]={}
+#        
+#        if pat.get_SUBJECT_id()[0] == 'NecS3Hs12':
+#            stdy1= pat.studies[0]
+#            stdy2= pat.studies[3]
+#        elif pat.get_SUBJECT_id()[0] == 'NecS3Hs04':
+#            stdy1= pat.studies[0]
+#            stdy2= pat.studies[2]
+#        elif len(pat.studies) != 2:
+#            print 'not 2 studies: %s ' % pat.get_SUBJECT_id()
+#            continue
+#        else:
+#            stdy1 = pat.studies[0]
+#            stdy2 = pat.studies[1]
+#
+#        master_sheet[nm[0]]['0h'] = stdy1.shortdirname
+#        master_sheet[nm[0]]['24h'] = stdy2.shortdirname
+#        
+#        #LL
+#        scn = stdy1.find_scan_by_protocol('04_')
+#        if len(scn) > 1:
+#            print 'more than one LL on day 1 %s' % len(scn) + scn[0].shortdirname
+#        master_sheet[nm[0]]['0h-LL']= scn[0].shortdirname
+#        scn = stdy2.find_scan_by_protocol('04_')
+#        if len(scn) > 1:
+#            print 'more than one LL on day 2 %s' % len(scn) + scn[0].shortdirname
+#        master_sheet[nm[0]]['24h-LL']= scn[0].shortdirname
+#
+#        #DCE 1 & 2         
+#        scn = stdy1.find_scan_by_protocol('06_')
+#        if len(scn) >= 2:
+#            master_sheet[nm[0]]['0h-DCE1']= magic_tuple(scn[-2])
+#            master_sheet[nm[0]]['0h-DCE2']= magic_tuple(scn[-1])
+#        elif len(scn) == 1:
+#            print '1 DCE on day 1 %s ' % stdy1.shortdirname
+#            
+#        scn = stdy2.find_scan_by_protocol('06_')
+#        if len(scn) >= 2:
+#            master_sheet[nm[0]]['24h-DCE1']= magic_tuple(scn[-2])
+#            master_sheet[nm[0]]['24h-DCE2']= magic_tuple(scn[-1])
+#        elif len(scn) ==1:
+#            print '1 DCE on day 2 %s ' % stdy1.shortdirname
+#          
+#         # RARE
+#        scn = stdy1.find_scan_by_protocol('05_.*RARE')
+#        if len(scn) == 2:
+#            master_sheet[nm[0]]['0h-IR_A']= magic_tuple(scn[0])
+#            master_sheet[nm[0]]['0h-IR_B']= magic_tuple(scn[1])
+#        elif len(scn) == 1:
+#            master_sheet[nm[0]]['0h-IR_A']= magic_tuple(scn[0])
+#        else:
+#            print 'RARE not clear, day1 %s ' % stdy1.shortdirname
+#            
+#        scn = stdy2.find_scan_by_protocol('05_.*RARE')
+#        if len(scn) == 2:
+#            master_sheet[nm[0]]['24h-IR_A']= magic_tuple(scn[0])
+#            master_sheet[nm[0]]['24h-IR_B']= magic_tuple(scn[1])
+#        elif len(scn) == 1:
+#            master_sheet[nm[0]]['24h-IR_A']= magic_tuple(scn[0])
+#        else:
+#            print 'RARE not clear, day2 %s ' % stdy2.shortdirname
+#          
+#          
+#         
+#        
+#    with open('/tmp/master_sheet.json','wb') as outfile:
+#        json.dump(master_sheet, outfile, indent=4)
+
+    
+    with open('/tmp/master_sheet.json','r') as infile:
+        x = json.load(infile)
+        
+        
+    for k,v in x.iteritems():
+        try:
+            print k, x[k]['24h-DCE2'][0]
+        except:
+            print k, 'too bad'
+    
