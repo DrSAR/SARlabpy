fd7bc1fa2facc54ef2f618b102e00852dfef5c2e
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-04-01 00:51:11 -0700

    first has at outsourcing the nifti1-header creation

--- sarpy/io/BRUKER_classes.py
+++ sarpy/io/BRUKER_classes.py
@@ -291,7 +291,14 @@ class PDATA_file(object):
 
         img_pair = nibabel.nifti1.Nifti1Image(data_copy,aff,header=header)
         img_pair.to_filename(filename)
+
+    def export2nii(self, filename):
+        from visu_pars_2_Nifti1Header import visu_pars_2_Nifti1Header
+        header = visu_pars_2_Nifti1Header(self.visu_pars)
+        img_pair = nibabel.nifti1.Nifti1Image(self.data, header=header)
+        img_pair.to_filename(filename)
         
+    
 class Scan(object):
     '''
     Object to represent a BRUKER scan consisting, typically, of FID_file 
