ce1a82c9a471f7414a89304f1613e76d2f62cc1d
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-04-08 22:18:45 -0700

    allow for unknown (default) keys

--- sarpy/io/dict_walk_minimal_example.py
+++ sarpy/io/dict_walk_minimal_example.py
@@ -4,7 +4,7 @@ Copyright: SARlab members, UBC, Vancouver, 2013
 """
 import re
 list_of_dicts = [{'s1':'abcd', 's2':'ABC', 'i':42, 'f':4.2},
-                 {'s2':'xyz', 'i':84, 'f':8.4}]
+                 {'s2':'xyz', 'i':84, 'f':8.4, 'notmentioned': 10}]
 
  # just making up some comparison strategies
 def regex_comp(a,b): return re.match(a,b)
@@ -13,13 +13,15 @@ def float_comp(a,b): return round(a,-1) == round (b,-1)
 
 pre_specified_comp_dict = {frozenset(['s1','s2']) : regex_comp,
                            frozenset(['i']): int_comp,
-                           frozenset(['f']): float_comp}
+                           frozenset(['f']): float_comp,
+                           frozenset(['default']): int_comp,}
 
 def fle_new(**kwargs):
     chosen_comps={}
     for key in kwargs.keys():
         # remember, the keys here are frozensets
-        cand_comp = [x for x in pre_specified_comp_dict if key in x]
+        cand_comp = [frozenset(['default'])] or [x for x in
+                                           pre_specified_comp_dict if key in x]
         chosen_comps[key] = pre_specified_comp_dict[cand_comp[0]]
 
     matches = lambda d: all(k in d and chosen_comps[k](v, d[k])
@@ -66,3 +68,5 @@ for method in [fle_orig,
     print 'string search {0}'.format(list(method(s2='xyz')))
     print 'regex search  {0}'.format(list(method(s2='[a-z]')))
     print 'keys combined {0}'.format(list(method(i=42, f=4.2)))
+    print 'key not known {0}'.format(list(method(notmentioned=1)))
+    print 'key not known {0}'.format(list(method(notmentioned=10)))
