49ad9f43bc5f32e6468dc1c25a9d25ff5e4ef87b
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-07-22 14:02:54 -0700

    nicer boards with labels above

--- sarpy/io/histoboard.py
+++ sarpy/io/histoboard.py
@@ -33,36 +33,45 @@ def natural_sort(l):
 
 conf_parser = argparse.ArgumentParser(
     # Turn off help, so we print all options in response to -h
-        add_help=False
+        add_help=False, 
+        # print script description with -h/--help
+        description=__doc__,
+        # Don't mess with format of description
+        formatter_class=argparse.RawDescriptionHelpFormatter
         )
 
 conf_parser.add_argument("-c", "--conf_file",
                          help="Specify config file", metavar="FILE")
 #parse_known_args does not produce an error for unknown args
 args, remaining_argv = conf_parser.parse_known_args()
+
 if args.conf_file:
     config = ConfigParser.SafeConfigParser()
+    config.optionxform = str
     config.read([args.conf_file])
     defaults = dict(config.items("Defaults"))
 else: 
-    defaults = {}
+    raise IOError('Need config file, provide filename with option -c\n'+
+                   '         see also --help')
 
 # Don't surpress add_help here so it will handle -h
 parser = argparse.ArgumentParser(
     # Inherit options from config_parser
     parents=[conf_parser],
-    # print script description with -h/--help
-    description=__doc__,
-    # Don't mess with format of description
-    formatter_class=argparse.RawDescriptionHelpFormatter,
     )
 parser.set_defaults(**defaults)
 parser.add_argument("--test","-t",default=False, action="store_true", 
                     help='dry run of tumour-board creation')
-parser.add_argument("--force",default=False, action="store_true", 
-                    help='ignore violations and attempt to run anyway')                    
-parser.add_argument("--scale",default=False, action="store_true", 
-                    help='create thumbnail of 5000 pixels width')                    
+parser.add_argument("--force", default=False, action="store_true", 
+                    help='ignore violations and attempt to run anyway')     
+parser.add_argument('--output', '-o', 
+                    help='output directory')
+parser.add_argument('--overwrite',
+                    help='overwrite existing files')                    
+parser.add_argument("--scale", default=False, action="store_true", 
+                    help='create thumbnail of factor pixels width')  
+parser.add_argument('--factor','-f', type=int,
+                    help='create thumbnail of factor pixels width')                      
 args = parser.parse_args(remaining_argv)
 
 markers = dict(config.items("Markers"))
@@ -74,7 +83,7 @@ if args.test:
 
 # slurp in directory names and check for inconsistencies.
 
-root = os.path.join(os.path.expanduser('~/hdata'), args.experimentname)
+root = os.path.join(os.path.expanduser('~/hdata'), args.ExperimentName)
 
 patient_slice_ID = set()
 for k,v in markers.iteritems():
@@ -91,7 +100,6 @@ for k,v in markers.iteritems():
 patients=set()
 violation = False
 for img_ID in patient_slice_ID:
-    #print img_ID
     img_ID_parsed = re.match('([0-9]+)([a-z])([0-9]+)([ab]*)', img_ID)
     if img_ID_parsed is None:
         print('convention violation (type I): %s' % img_ID)
@@ -124,23 +132,60 @@ for patient in patients:
     # find all the images for the current patient
     imges = [x for x in patient_ID_sorted 
                         if re.match(patient+'[a-z].*', x) is not None]                
-    tmpfiles = []
+    histo_row_files = []
     for k,v in markers.iteritems():
         img_fnames = [glob.glob(os.path.join(root, v, x+'*jpg'))[0] for x in imges]
         print patient, v
-        tmpfiles.append(tempfile.mktemp(prefix='histoboardtmp', suffix='.png'))
-        cmd = 'convert +append {0} {1}'.format(
-                        ' '.join([shellquote(x) for x in img_fnames]),
-                        tmpfiles[-1])
-        print(cmd)
+        
+        histo_row_files.append(os.path.join(args.output, patient+'-'+k+'.jpg'))
+        
+        # check whether file exists and don't bother recreating if it present
+        if os.path.exists(histo_row_files[-1]) and not args.overwrite:
+            print('previously created file found: %s' % histo_row_files[-1])
+        else:
+            # annotate file
+            labelled_img_fnames=[]
+            for image_file in img_fnames:
+                lbl = os.path.basename(image_file).split()[0]
+                #cmd = 'convert -size 100x14 xc:none -gravity center '+ \
+                #      '-stroke black -strokewidth 2 -annotate 0 "%s"' % lbl + \
+                #      '-background none -shadow 100x3+0+0 +repage ' +\
+                #      '-stroke none -fill white -annotate 0 "%s" ' % lbl + \
+                #      "'%s' " % image_file +'+swap -gravity south ' + \
+                #      ' -geometry +0-3 -composite '+\
+                #      "'%s.jpg'" % os.path.join(args.output, lbl+'.png')
+                labelled_outfile = os.path.join(args.output, lbl+'.jpg')
+                cmd = "convert %s -pointsize 155 " % shellquote(image_file) + \
+                      "label:'%s' +swap -gravity Center " % lbl+ \
+                      "-append %s" %  shellquote(labelled_outfile)
+                labelled_img_fnames.append(labelled_outfile)
+                print cmd
+                os.system(cmd)      
+            cmd = 'convert +append {0} {1}'.format(
+                        ' '.join([shellquote(x) for x in labelled_img_fnames]),
+                        histo_row_files[-1])
+            print('running: %s' % cmd)
+            if not args.test: os.system(cmd)
+            for f in labelled_img_fnames:
+                print('removing %s' % f)
+                os.remove(f)
+                
+    largeoutfile = os.path.join(args.output, patient+'.jpg')
+    cmd = 'convert -append {0} {1}'.format(
+                      ' '.join([shellquote(x) for x in histo_row_files]), 
+                      largeoutfile)
+    if os.path.exists(largeoutfile) and not args.overwrite:
+        print('not running: %s' % cmd)
+    else:
+        print('running: %s' % cmd)
         if not args.test: os.system(cmd)
         
-    cmd = 'convert -append {0} {1}'.format(' '.join(tmpfiles), patient+'.png')
-    print(cmd)        
-    if not args.test: 
-        os.system(cmd)
-        if args.scale:
-            cmd = 'convert -scale 5000 {0} {1}'.format(patient+'.png', patient+'-scaled.png')
-            print(cmd)        
-            os.system(cmd)        
-        
+    if args.scale:
+        outfile = os.path.join(args.output, patient+'-width%s.jpg' % args.factor)
+        cmd = 'convert -scale {0} {1} {2}'.format(
+                            args.factor, largeoutfile, outfile)
+        if os.path.exists(outfile) and not args.overwrite:
+            print('not running: %s' % cmd)
+        else:
+            print('running: %s' % cmd)
+            if not args.test: os.system(cmd)
