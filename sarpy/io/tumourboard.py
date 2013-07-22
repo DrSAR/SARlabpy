0a98d30d5e2f578c0928ff818112fb65f7b66ee9
Firas Moosvi <firas@moosvi.com>
2013-07-21 19:57:55 -0700

    minor change to tumour board to allow for a fringe case when creating a tumour board.
    added success message to wrappers.

--- sarpy/io/tumourboard.py
+++ sarpy/io/tumourboard.py
@@ -47,9 +47,9 @@ def determine_figure_size(n_rows,n_cols):
     elif n_rows >=8 and n_cols <=10:
         #moderately tested        
         figure_size = (16,8*aspect)
-        font_modifier = 6
+        font_modifier = 6      
 
-    elif n_rows > 8 and n_cols > 10:
+    elif n_rows >= 6 and n_cols > 10:
 
         figure_size = (18,19*aspect)
         font_modifier = 6
