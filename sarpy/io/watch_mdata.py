cab41b037fd33f3eb10e7771ef6a71abe3b0f2bb
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-07-25 18:36:09 -0700

    add watch_mdata: daemon to observe and react to changes in ~/mdata

---
+++ sarpy/io/watch_mdata.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Watch directory and trigger processing
+
+@author: stefan a reinsberg
+Created on Thu Jul 25 16:36:34 2013
+"""
+
+import os
+import pyinotify
+import datetime
+import time
+import hashlib
+
+wm = pyinotify.WatchManager()
+# watched events
+mask = pyinotify.IN_MODIFY
+
+class PTmp(pyinotify.ProcessEvent):
+    def process_IN_CREATE(self, event):
+        print "Create: %s" %  os.path.join(event.path, event.name)
+    def process_IN_DELETE(self, event):
+        print "Remove: %s" %  os.path.join(event.path, event.name)
+    def process_IN_MODIFY(self, event):
+        fname = os.path.join(event.path, event.name)
+        print "Modify: {0} ({1})".format(fname,
+                                         datetime.datetime.now().strftime('%X'))
+        with open(fname, 'r') as f_in:
+            lines = filter(None, (line.rstrip() for line in f_in))
+        digest = hashlib.md5(lines).hexdigest()
+        
+        
+
+
+notifier = pyinotify.Notifier(wm, PTmp())
+
+wdd = wm.add_watch(os.path.expanduser('/srv/mdata'), mask, rec=True)
+
+while True:
+    try:
+        # process the queue of events as explained above
+        notifier.process_events()
+        if notifier.check_events():
+            # read notified events and enqeue them
+            notifier.read_events()
+        # you can do some tasks here...
+        
+    except KeyboardInterrupt:
+        # destroy the inotify's instance on this interrupt (stop monitoring)
+        notifier.stop()
+        print('Keyboard interrupt detected')
+        break
