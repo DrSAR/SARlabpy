8cb5a45150ed5a32cef6d29738114c38cce3433c
Firas Moosvi <firas@moosvi.com>
2013-08-01 16:25:15 -0700

    Expand to make it runnable from command line, change variable names for consistency with generate_roi

--- sarpy/io/write_csv.py
+++ sarpy/io/write_csv.py
@@ -5,7 +5,7 @@ Created on Thu Aug  1 13:53:03 2013
 
 @author: fmoosvi
 """
-def write_csv(masterlist, scan_label, adata_label):
+def write_csv(masterlist_name, data_label, adata_label):
     
     import json
     import os
@@ -13,10 +13,12 @@ def write_csv(masterlist, scan_label, adata_label):
     import csv
     import scipy.stats
     import sarpy
+    import time
+    import getpass # used to get the current username
     
     root = os.path.join(os.path.expanduser('~/sdata'),
-                        masterlist,
-                        masterlist)
+                        masterlist_name,
+                        masterlist_name)
     fname_to_open = root+'.json'
     
     with open(os.path.join(fname_to_open),'r') as master_file:
@@ -30,24 +32,31 @@ def write_csv(masterlist, scan_label, adata_label):
     for k,v in master_list.iteritems():
         
         try:        
-            data = sarpy.Scan(v[scan_label][0]).adata[adata_label].data
+            data = sarpy.Scan(v[data_label][0]).adata[adata_label].data
             avg = scipy.stats.nanmean(data)
 
         except KeyError:
             print('write_csv: Not found {0} and {1},{2}'.format(
-                                                    k,scan_label,adata_label) )
+                                                    k,data_label,adata_label) )
             data = numpy.empty([1])*nan                                     
             avg = 'AnalysisErr'
             
             pass
 
         export_data.append([k] + data.tolist() + [avg])
-#        print export_data
-        				       
-    header = ['Animal ID'] + ['Slice '+str(x+1) for x in xrange(
+
+    time = time.strftime("%Y-%m-%d %H:%M")       				       
+    header =['Animal ID'] + ['Slice '+str(x+1) for x in xrange(
                                         len(export_data[0])-2)] + ['Average']
 
-    filename = scan_label + '_' + adata_label + '.csv'
+    footer =  ['# Data generated on {0} by {1}'.format(time,getpass.getuser())]
+    
+    export_data.append(footer)    
+    
+
+    filename = os.path.expanduser(os.path.join(
+                                          '~','sdata',masterlist_name,'export',
+                                          data_label+'_'+adata_label+'.csv'))
    
     # open output file
     outfile = open(filename, "wb" )
@@ -63,7 +72,31 @@ def write_csv(masterlist, scan_label, adata_label):
     
     # close file
     outfile.close()
+       
+if __name__ == '__main__':
+
+    import argparse
+    import os
+
+    # we have been called as a script
+    parser = argparse.ArgumentParser()
     
-write_csv('HerS10', '0h-LL', 'T1avg')
+    parser.add_argument('-m', '--masterlist_name', type=str, required=True,
+                       help='Name of the masterlist file. e.g. NecS3 \
+                       Usage: python write_csv.py -m HerP2 -d axref -a roi -i export ')
     
-# if __name__ == '__main__':    
+    parser.add_argument('-d', '--data_label', type=str, required=True,
+                       help='Data label, usually anatomy or IR_A')
+
+    parser.add_argument('-a', '--adata_label', type=str,required=True,
+                       help='Data label, usually anatomy or IR_A')                          
+                       
+    args = parser.parse_args()
+    masterlist_name = args.masterlist_name
+    data_label = args.data_label
+    adata_label = args.adata_label
+    
+    
+    write_csv(masterlist_name,data_label,adata_label)
+    
+    
