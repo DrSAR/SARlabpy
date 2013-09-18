bce2b053f62a2f7bb9e1bfef5848b6478620c0ea
Firas Moosvi <firas@moosvi.com>
2013-09-17 18:30:16 -0700

    rebase gone bad (merge conflicts, things ended up in random spots)…. copied the analysis file contents from iss183 branch and then manually made the (small) changes to get the other ones.

--- sarpy/fmoosvi/analysis.py
+++ sarpy/fmoosvi/analysis.py
@@ -244,89 +244,6 @@ def h_inj_point(scan_object, pdata_num = 0):
 
     return injection_point[0][0]+1
 
-def h_conc_from_signal(scan_object, scan_object_T1map, 
-                       adata_label = 'T1map_LL', bbox = None,
-                       relaxivity=4.3e-3, pdata_num = 0):
-
-    ########### Getting and defining parameters
-    
-    # Data
-    data = scan_object.pdata[pdata_num].data
-    
-    # resample the t1map onto the dce
-    data_t1map_pre = scan_object_T1map.adata[adata_label]
-    
-    data_t1map = sarpy.ImageProcessing.resample_onto.resample_onto_pdata(data_t1map_pre,scan_object.pdata[pdata_num],use_source_dims=True)
-
-    x_size = data.shape[0]
-    y_size = data.shape[1]
-    num_slices = getters.get_num_slices(scan_object,pdata_num)
-    
-    # Method params
-    #TODO: change this so it doesn't require method file WIHOUT BREAKING IT!
-    reps =  scan_object.method.PVM_NRepetitions
-
-    if reps != scan_object.pdata[pdata_num].data.shape[-1]:
-        reps = scan_object.pdata[pdata_num].data.shape[-1]
-        print('\n \n ***** Warning **** \n \n !!! Incomplete dce data for {0} \n \n'.format(scan_object.shortdirname) )
-    
-    
-    TR = scan_object.method.PVM_RepetitionTime
-    FA = scan_object.acqp.ACQ_flip_angle
-    
-    inj_point = sarpy.fmoosvi.analysis.h_inj_point(scan_object, pdata_num = 0)    
-    
-    # Deal with bounding boxes
-    if bbox is None:        
-        bbox = numpy.array([0,x_size-1,0,y_size-1])    
-       
-    if bbox.shape == (4,):            
-    
-        bbox_mask = numpy.empty([x_size,y_size])
-        bbox_mask[:] = numpy.nan        
-        bbox_mask[bbox[0]:bbox[1],bbox[2]:bbox[3]] = 1
-    
-        # First tile for slice
-        bbox_mask = numpy.tile(bbox_mask.reshape(x_size,y_size,1),num_slices)
-
-    else:      
-        raise ValueError('Please supply a bbox for h_conc_from_signal')   
-
-    T1 = numpy.empty([x_size,y_size,num_slices,reps])
-    T1[:] = numpy.nan
-    
-    for x in xrange(bbox[0],bbox[1]):
-        for y in xrange(bbox[2],bbox[3]):
-            for slice in xrange(num_slices):
-                               
-                baseline_s = numpy.mean(data[x,y,slice,0:inj_point])
-                E1 = numpy.exp(-TR/data_t1map[x,y,slice])
-                c = numpy.cos(numpy.radians(FA))
-                
-                T1[x,y,slice,0:inj_point] = data_t1map[x,y,slice]
-                
-# Use the SPGR equation twice, once before agent admin. and once after. If you
-# divide the two, the M0s cancel out, and so do the sin thetas. what remains is:
-# (1-E1)*(1-E0*cos) / ((1-E0) * (1-E1*cos)). use wolfram alpha to solve this:
-# http://www.wolframalpha.com/input/?i=solve+%28%281-x%29*%281-b*c%29%29+%2F+%28%281-b%29*%281-x*c%29%29+%3D+r+for+x
-
-                for rep in xrange(inj_point,reps):                    
-                    s = data[x,y,slice,rep] / baseline_s
-                    E2 = (-E1*c + E1*s - s + 1) / (E1*s*c - E1*c - s*c +1)
-                    
-                    T1[x,y,slice,inj_point:] = -TR / numpy.log(E2)
-
-    # If this gives a value error about operands not being broadcast together, go backand change your adata to make sure it is squeezed
-
-    T1baseline = numpy.squeeze(data_t1map)*bbox_mask
-    T1baseline = numpy.tile(T1baseline.reshape(x_size,y_size,num_slices,1),reps)
-    conc = (1/relaxivity) * ( (1/T1) - (1/T1baseline) )
-    
-    conc[conc<0] = 0
-       
-    return conc
-
-
 def h_calculate_AUGC(scan_object, adata_label, bbox = None, time = 60, pdata_num = 0):
     
     """
@@ -411,6 +328,86 @@ def h_calculate_AUGC(scan_object, adata_label, bbox = None, time = 60, pdata_num
         # If this gives a value error about operands not being broadcast together, go backand change your adata to make sure it is squeezed
     return augc_data*bbox_mask     
     
+def h_conc_from_signal(scan_object, scan_object_T1map, 
+                       adata_label = 'T1map_LL', bbox = None,
+                       relaxivity=4.3e-3, pdata_num = 0):
+
+    ########### Getting and defining parameters
+    
+    # Data
+    data = scan_object.pdata[pdata_num].data
+    
+    # resample the t1map onto the dce
+    data_t1map_pre = scan_object_T1map.adata[adata_label]
+    
+    data_t1map = sarpy.ImageProcessing.resample_onto.resample_onto_pdata(data_t1map_pre,scan_object.pdata[pdata_num],use_source_dims=True)
+
+    x_size = data.shape[0]
+    y_size = data.shape[1]
+    num_slices = getters.get_num_slices(scan_object,pdata_num)
+    
+    # Method params
+    #TODO: change this so it doesn't require method file WIHOUT BREAKING IT!
+    reps =  scan_object.method.PVM_NRepetitions
+
+    if reps != scan_object.pdata[pdata_num].data.shape[-1]:
+        reps = scan_object.pdata[pdata_num].data.shape[-1]
+        print('\n \n ***** Warning **** \n \n !!! Incomplete dce data for {0} \n \n'.format(scan_object.shortdirname) )
+    
+    
+    TR = scan_object.method.PVM_RepetitionTime
+    FA = scan_object.acqp.ACQ_flip_angle
+    
+    inj_point = sarpy.fmoosvi.analysis.h_inj_point(scan_object, pdata_num = 0)    
+    
+    # Deal with bounding boxes
+    if bbox is None:        
+        bbox = numpy.array([0,x_size-1,0,y_size-1])    
+       
+    if bbox.shape == (4,):            
+    
+        bbox_mask = numpy.empty([x_size,y_size])
+        bbox_mask[:] = numpy.nan        
+        bbox_mask[bbox[0]:bbox[1],bbox[2]:bbox[3]] = 1
+    
+        # First tile for slice
+        bbox_mask = numpy.tile(bbox_mask.reshape(x_size,y_size,1),num_slices)
+
+    else:      
+        raise ValueError('Please supply a bbox for h_conc_from_signal')   
+
+    T1 = numpy.empty([x_size,y_size,num_slices,reps])
+    T1[:] = numpy.nan
+    
+    for x in xrange(bbox[0],bbox[1]):
+        for y in xrange(bbox[2],bbox[3]):
+            for slice in xrange(num_slices):
+                               
+                baseline_s = numpy.mean(data[x,y,slice,0:inj_point])
+                E1 = numpy.exp(-TR/data_t1map[x,y,slice])
+                c = numpy.cos(numpy.radians(FA))
+                
+                T1[x,y,slice,0:inj_point] = data_t1map[x,y,slice]
+                
+# Use the SPGR equation twice, once before agent admin. and once after. If you
+# divide the two, the M0s cancel out, and so do the sin thetas. what remains is:
+# (1-E1)*(1-E0*cos) / ((1-E0) * (1-E1*cos)). use wolfram alpha to solve this:
+# http://www.wolframalpha.com/input/?i=solve+%28%281-x%29*%281-b*c%29%29+%2F+%28%281-b%29*%281-x*c%29%29+%3D+r+for+x
+
+                for rep in xrange(inj_point,reps):                    
+                    s = data[x,y,slice,rep] / baseline_s
+                    E2 = (-E1*c + E1*s - s + 1) / (E1*s*c - E1*c - s*c +1)
+                    T1[x,y,slice,rep] = -TR / numpy.log(E2)
+
+    # If this gives a value error about operands not being broadcast together, go backand change your adata to make sure it is squeezed
+
+    T1baseline = numpy.squeeze(data_t1map)*bbox_mask
+    T1baseline = numpy.tile(T1baseline.reshape(x_size,y_size,num_slices,1),reps)
+    conc = (1/relaxivity) * ( (1/T1) - (1/T1baseline) )
+    
+    conc[conc<0] = 0
+       
+    return conc
 
 def h_fit_PK(scan_object, 
              adata_label,
@@ -536,16 +533,7 @@ def h_fit_PK(scan_object,
 
 
 
-                for rep in xrange(inj_point,reps):                    
-                    s = data[x,y,slice,rep] / baseline_s
-                    E2 = (-E1*c + E1*s - s + 1) / (E1*s*c - E1*c - s*c +1)
-                    T1[x,y,slice,rep] = -TR / numpy.log(E2)
-
-
-
-
-
-
+    
     
 ### T1 fitting section
     
@@ -617,7 +605,7 @@ def h_fit_T1_LL(scan_object, bbox = None, flip_angle_map = 0, pdata_num = 0,
     data_after_fitting = numpy.zeros( [data.shape[0],\
                                        data.shape[1],\
                                        data.shape[2]] )
-                                           
+                                       
     repetition_time = scan_object.method.PVM_RepetitionTime
     inversion_time = scan_object.method.PVM_InversionTime   
     x_size = data.shape[0]
