f5d1dd4ec7a2cb5ca3be623cc8cf386af4c09058
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-09-24 22:21:40 -0700

    changed to object based approach example now provided

--- sarpy/io/bulk_analyze.py
+++ sarpy/io/bulk_analyze.py
@@ -13,33 +13,52 @@ import os
 import collections
 import json
 
-def bulk_analyze(master_list_fname,
-                 scan_criterion,
-                 scan_params,
-                 analysis_func):
+
+class bulk_analyzer(object):
+    def __init__(self,
+                 master_list_fname):
 
     # open masterlist and iterate over all patients and scans
+        with open(os.path.join(os.path.expanduser('~/sdata'),
+                               master_list_fname),'r') as master_file:
+            json_str = master_file.read()
+            self.master_list = json.JSONDecoder(
+                            object_pairs_hook=collections.OrderedDict).decode(json_str)
+    # this is where we keep a record of the results
+        self.results={}
 
-    with open(os.path.join(os.path.expanduser('~/sdata'),master_list_fname),'r') as master_file:
-        json_str = master_file.read()
-        master_list = json.JSONDecoder(object_pairs_hook=collections.OrderedDict
-                                      ).decode(json_str)    
+    def scan_criterion(self, scan_fname):
+        return true
 
-    for pat_lbl, pat in master_list.iterlist():
-        for scn_lbl, scn_fname in pat.iteritems():
-            if scan_criterion(scn_fname):
-                # this is a scan we should analyze!
-                kwargs = scan_params(scn)
-                analysis_func(scn, **kwargs)
+    def process_params(self, scan_fname):
+        return {}
+   
+    def analysis_func(self, scan_fname):
+        return 0
 
+    def process(self):
+        for pat_lbl, pat in self.master_list.iterlist():
+            for scn_lbl, scn_fname in pat.iteritems():
+                if self.scan_criterion(scn_fname):
+                    # this is a scan we should analyze!
+                    kwargs = scan_params(scn)
+                    self.results[scn_lbl] = self.analysis_func(scn, **kwargs)
+        return self.results
 
 import re
 import sarpy
 
 # below are example function that achieve the minimum for a run of analysis
-def this_is_dce(fname):
-    scn = sarpy.Scan(fname)
-    if re.search('.*DCE',fname.acqp.protocol_name):
-        return True
-    else:
-        return False
+class dce_NR_counter(bulk_analyzer): # we inherit from the generic analyzer
+                                     # object
+    def scan_criterioni(self, scan_fname):
+        scn = sarpy.Scan(scan_fname)
+        if re.search('.*DCE',scn.acqp.protocol_name):
+            return True
+        else:
+            return False
+    def analysis_func(self, scan_fname):
+        return scn.acqp.NR
+
+res = dce_NR_counter('NecS3').process()
+
