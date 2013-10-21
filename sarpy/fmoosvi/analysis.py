fc5a74dcd450246d503f8fb32f7d84a993628c92
Firas Moosvi <firas@moosvi.com>
2013-10-20 22:08:10 -0700

    changed scn to scan

--- sarpy/fmoosvi/analysis.py
+++ sarpy/fmoosvi/analysis.py
@@ -338,7 +338,7 @@ def h_calculate_AUGC(scn_to_analyse=None, adata_label=None, bbox = None, time = 
         # If this gives a value error about operands not being broadcast together, go backand change your adata to make sure it is squeezed
     return augc_data*bbox_mask     
     
-def h_conc_from_signal(scn_to_analyse=None, other_scn_name=None, 
+def h_conc_from_signal(scn_to_analyse=None, other_scan_name=None, 
                        adata_label = 'T1map_LL', bbox = None,
                        relaxivity=4.3e-3, pdata_num = 0):
 
