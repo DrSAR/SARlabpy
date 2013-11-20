30e1494d4676000d7be37fec93921cd1e689c4d8
Firas Moosvi <firas@moosvi.com>
2013-11-20 00:10:19 -0800

    minor updates to watch_sdata

--- sarpy/io/watch_sdata.py
+++ sarpy/io/watch_sdata.py
@@ -11,6 +11,7 @@ import pyinotify
 import datetime
 import time
 import hashlib
+import sarpy.io
 
 wm = pyinotify.WatchManager()
 # watched events
@@ -28,21 +29,21 @@ class PTmp(pyinotify.ProcessEvent):
         with open(fname, 'r') as f_in:
             lines = filter(None, (line.rstrip() for line in f_in))
         digest = hashlib.md5(lines).hexdigest()
-        
-        
 
-
+        sarpy.io.mriBoard.generate(fname)
+        
 notifier = pyinotify.Notifier(wm, PTmp())
 
-wdd = wm.add_watch(os.path.expanduser('/srv/sdata'), mask, rec=True)
+wdd = wm.add_watch(os.path.join(os.path.expanduser('~'),'sdata','mriBoards'), mask, rec=True)
 
 while True:
     try:
         # process the queue of events as explained above
         notifier.process_events()
         if notifier.check_events():
-            # read notified events and enqeue them
+            # read notified events and enqueue them
             notifier.read_events()
+            raise NameError
         # you can do some tasks here...
         
     except KeyboardInterrupt:
