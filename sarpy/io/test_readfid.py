62a748765b70df98abec9f2c2b42f8194189cf41
Stefan A Reinsberg <stefan@phas.ubc.ca>
2013-11-21 18:18:23 -0800

    avoid KeyError when retrieving some acqp parameters

--- sarpy/io/test_readfid.py
+++ sarpy/io/test_readfid.py
@@ -11,22 +11,25 @@ import pprint
 
 files =  ['~/bdata/stefan/nmr/readfidTest.ix1/%i/fid' % (i+1) for i 
          in range(18)+[98]]
+files = ['~/bdata/stefan/nmr/NecS3Hs02.iJ1/10/fid']         
 
 for fid_file in files:
     print fid_file+'\n'+'-'*50
     try:
-        fid = BRUKERIO.readfid(os.path.expanduser(fid_file))
+        fid = BRUKERIO.readfid(os.path.expanduser(fid_file), untouched=False)
     except Exception, ex:
+        print 'EXCEPTION'
         print ex
     else:
-        
-        print fid['data'].shape
-        
-        pprint.pprint(['%s: %s' % (k,fid['header']['acqp'][k]) for 
-               k in ['ACQ_dim','ACQ_size','GO_block_size','NI','NSLICES','NR',
-                     'ACQ_obj_order','ACQ_phase_factor',
-                     'ACQ_phase_encoding_mode',
-                     'ACQ_phase_enc_start','ACQ_rare_factor',
-                     'ACQ_spatial_phase_1','ACQ_spatial_size_1']])
+        print 'no EXCEPTION'
+            
+    print fid['data'].shape
+    
+    pprint.pprint(['%s: %s' % (k,(fid['header']['acqp']).get(k, 'MISSING')) for 
+           k in ['ACQ_dim','ACQ_size','GO_block_size','NI','NSLICES','NR',
+                 'ACQ_obj_order','ACQ_phase_factor',
+                 'ACQ_phase_encoding_mode',
+                 'ACQ_phase_enc_start','ACQ_rare_factor',
+                 'ACQ_spatial_phase_1','ACQ_spatial_size_1']])
 
     print '\n'+'='*50
