b1727d5215abe98764472259456b1c17e64d6e06
Stefan A Reinsberg <stefan@phas.ubc.ca>
2014-04-29 22:49:57 -0700

    fast and quick routine to get histogram (dict of 1D vector)

---
+++ sarpy/analysis/get_histogram.py
@@ -0,0 +1,42 @@
+def get_histogram(masterlist,
+                  pat_regex='', # the empty regex fits all
+                  scan_regex='',
+                  adata_regex='',
+                  slice_range=None,
+                  roi_label=None, # tupel containing scan and adata label
+                  verbose=False):
+    '''Returns histogram for a parameter map inside a given ROI
+    
+    HerP2_dR1_HPG=get_histogram(masterlist, pat_regex='',
+                                scan_regex='',
+                                adata_regex='multiday_deltaR1_HPG',
+                                slice_range=None,
+                                roi_label=('roi-24h', 'roi'),
+                                verbose=True)
+    '''
+    
+    vals = {}
+    for pat in [x for x in masterlist if re.search(pat_regex, x)]:
+        if verbose: print('looking at {0}'.format(pat))
+        for scn_lbl in [x for x in masterlist[pat] if re.search(scan_regex, x)]:
+            if verbose: print('- scan {0}'.format(scn_lbl))
+            try:
+                scn = sarpy.Scan(masterlist[pat][scn_lbl][0])
+            except IOError:
+                if verbose: print('!! could not find data for {0}'.format(scn_lbl))
+            else:
+                for adata_lbl in [x for x in scn.adata.keys() if re.search(adata_regex, x)]:
+                    target_data = scn.adata[adata_lbl].data
+                    if verbose: print('  - adata {0} {1}'.format(adata_lbl,target_data.shape))
+                    if roi_label is not None:
+                        # mask the target_data with a ROI mask
+                        scn_roi = sarpy.Scan(masterlist[pat][roi_label[0]][0])
+                        roi = scn_roi.adata[roi_label[1]].data
+                        target_data = target_data * roi
+                        
+                    if slice_range is None:
+                        slice_range = numpy.arange(scn.adata[adata_lbl].data.shape[-1])
+                    xx = target_data[:,:,slice_range]
+                    vals[pat]=xx[numpy.where(numpy.isfinite(xx))]
+    return vals
+
