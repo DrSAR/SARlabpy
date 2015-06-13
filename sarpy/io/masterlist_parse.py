a9586625987716c983adb14d0ee9f14c8b1b8a86
Firas Moosvi <firas@moosvi.com>
2015-06-13 01:23:43 -0400

    coded in
    get_scans_from_masterlist(BrukerObject,scan_label_list)

--- sarpy/io/masterlist_parse.py
+++ sarpy/io/masterlist_parse.py
@@ -1,7 +1,7 @@
 import sarpy.helpers
+import os
 
-def masterlist_parse(BrukerObject,
-					 ):
+def masterlist_parse(BrukerObject):
 
 	'''parse the file and:
 
@@ -57,4 +57,29 @@ def masterlist_parse(BrukerObject,
 
 
 	return studies, studies_dict, scanlabels_dict,scanlabels
+
+
+def get_scans_from_masterlist(BrukerObject,scan_label_list):
+
+	if type(scan_label_list) is str:
+
+		tmp = []
+		tmp.append(scan_label_list)
+		scan_label_list = tmp
+
+		print('String converted to list of size 1 \n Next time, please supply a proper list')
+
+	a,b,scanlabels_dict,d = masterlist_parse(BrukerObject)
+	scan_dict = {}
+
+	for scan_label in scan_label_list:
+
+		scan_dict[scan_label] = []
+
+		for k,v in scanlabels_dict.iteritems():
+			for stud in v:
+				if scan_label in v[stud].keys():
+					scan_dict[scan_label].append(os.path.join(k+'.'+stud.strip('study ')+'/',v[stud][scan_label]))	
+
+	return scan_dict
 
