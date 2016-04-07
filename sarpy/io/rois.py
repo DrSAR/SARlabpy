4643df9606e3c5b7db50ae4dedd52b090c0770c3
Firas Moosvi <firas@moosvi.com>
2016-04-07 11:37:42 -0700

    refactored rois for exporting and importing. added more comments so that it makes more sense.

--- sarpy/io/rois.py
+++ sarpy/io/rois.py
@@ -10,6 +10,7 @@ Created on Thu May 30 17:49:35 2013
 import sarpy.fmoosvi.getters
 
 def export_roi(masterlist_name,
+               roi_scan_label = None,
                roi_suffix = None,
                path = None,
                rescale = None,
@@ -45,9 +46,13 @@ def export_roi(masterlist_name,
     exp = sarpy.Experiment.from_masterlist(masterlist_name+'.config')
 
     for k in exp.patients.keys():
-        # Search for all the labels that have roi and create a scan label list of rois
 
-        roi_labels = [r for r in exp.labels.keys() if 'roi' in r]
+        if roi_scan_label is None:
+            # Search for all the labels that have roi and create a scan label list of rois
+            roi_labels = [r for r in exp.labels.keys() if 'roi' in r]
+        else:
+            # This overrides in case there wasn't an explict roi_scan acquired.
+            roi_labels = [roi_scan_label]
 
         # Iterate over the roi list, import or export the rois
         for r_lbl in roi_labels:
@@ -55,25 +60,26 @@ def export_roi(masterlist_name,
             try:
                 scn_name = exp.patients[k][r_lbl]
                 scan = sarpy.Scan(scn_name)
+
+                sdir = scn_name.replace('/','_')  
+
+                fname = os.path.join(path, sdir + '.nii')
+
+                # Export the image as a niftii file            
+
+                # Note: std_modifier is a value that controls scaling of the image 
+                # Look at code in getters.get_image_clims(data,std_modifier=None):
+                # Basically controls the upper limit of the default image scaling
+
+                scan.pdata[0].export2nii(fname,rescale,std_modifier)                
                 
-            except IOError:    
-                print('\n \n ** WARNING ** \n \n Not found: {0} and {1} \n'.format(k,r_lbl) )
-                continue
-
-            sdir = scn_name.replace('/','_')  
-
-            fname = os.path.join(path, sdir + '.nii')
-
-            # Export the image as a niftii file            
-
-            # Note: std_modifier is a value that controls scaling of the image 
-            # Look at code in getters.get_image_clims(data,std_modifier=None):
-            # Basically controls the upper limit of the default image scaling
-
-            scan.pdata[0].export2nii(fname,rescale,std_modifier)
+            except(IOError,KeyError):    
+                print('\n \n ** WARNING ** \n \n Not found (check permissions): {0} and {1} \n'.format(k,r_lbl) )
+                pass
 
 def import_roi(masterlist_name,
                roi_suffix = None,
+               roi_scan_label = None,
                path = None,
                rescale = None,
                std_modifier = None,
@@ -109,9 +115,12 @@ def import_roi(masterlist_name,
     exp = sarpy.Experiment.from_masterlist(masterlist_name+'.config')
 
     for k in exp.patients.keys():
-        # Search for all the labels that have roi and create a scan label list of rois
-
-        roi_labels = [r for r in exp.labels.keys() if 'roi' in r]
+        if roi_scan_label is None:
+            # Search for all the labels that have roi and create a scan label list of rois
+            roi_labels = [r for r in exp.labels.keys() if 'roi' in r]
+        else:
+            # This overrides in case there wasn't an explict roi_scan acquired.
+            roi_labels = [roi_scan_label]
 
         # Iterate over the roi list, import or export the rois
         for r_lbl in roi_labels:
@@ -120,7 +129,7 @@ def import_roi(masterlist_name,
                 scn_name = exp.patients[k][r_lbl]
                 scan = sarpy.Scan(scn_name)
                 
-            except IOError:    
+            except(IOError,KeyError):    
                 print('\n \n ** WARNING ** \n \n Not found: {0} and {1} \n'.format(k,r_lbl) )
                 continue
 
@@ -139,6 +148,14 @@ def import_roi(masterlist_name,
             except IndexError:
                 roi = nibabel.load(fname).get_data()[:,:,:]
 
+            # Hack for CEST EPI scans because you cannot really draw ROI on any other image
+            # So basically I had to export the CEST EPI scan with 1 slice and ~80 freqs as the ROI source
+            # This line takes just the first slice where the tumour is drawn. If multislice CEST ever becomes a 
+            # TODO: reality, that will need to be taken care of.
+
+            if sarpy.fmoosvi.getters.get_num_slices(scn_name) ==1 and len(roi.shape) > 2:
+                roi = roi[:,:,0]
+
             # the default foreground and background in h_image_to_mask
             # will result in a roi_m that has NaN and 1 only (aka
             # 'proper mask')
@@ -148,8 +165,11 @@ def import_roi(masterlist_name,
             weights = numpy.zeros(shape=roi_m.shape[-1])
             ROIpx = numpy.nansum(roi_m.flatten())
 
-            for sl in xrange(roi_m.shape[-1]):
-                weights[sl] = numpy.divide(numpy.nansum(roi_m[:,:,sl].flatten()),ROIpx)
+            if len(roi_m.shape) < 3:
+                weights = [1.0]
+            else:
+                for sl in xrange(roi_m.shape[-1]):
+                    weights[sl] = numpy.divide(numpy.nansum(roi_m[:,:,sl].flatten()),ROIpx)
 
             try:
                 scan.store_adata(key=roi_key_name, data = roi_m, force = forceVal)
@@ -170,14 +190,49 @@ def import_roi(masterlist_name,
 
         pat = exp.patients[k]
 
+        # The code assumes that there is a scanlabel called roi, fixed so that it can be overridden
+        # Still need to fix this !
+
+        if roi_scan_label is None:
+            roi_scan_label = 'roi'
         try:
-            bbox = sarpy.Scan(pat['roi']).adata['bbox'].data
+            #bbox = sarpy.Scan(pat[roi_scan_label]).adata['bbox'].data
+            bbox = sarpy.fmoosvi.getters.get_roi_bbox(scn,roi_scan_label)
 
             for lbl,scn in pat.iteritems():
 
                 scn = sarpy.Scan(scn)
                 scn.store_adata(key='bbox', data = bbox,force=True)
         except:
-            raise
+            print('No ROI exists for: {0}'.format(k))
 
     print('Nifti images were processed in {0}'.format(path))
+
+def transfer_roi(masterlist_name,
+                 roi_lbl, 
+                 scn_lbl, 
+                 roi_adata = None, 
+                 forceVal = False,
+                 adata_save_lbl = None):
+
+    if adata_save_lbl is None:
+        adata_save_lbl = 'transferred_roi'
+
+    if roi_adata is None:
+        roi_adata = 'roi'
+
+    exp = sarpy.Experiment.from_masterlist(masterlist_name+'.config')
+
+    for scn,roi_scn in zip(exp.labels[scn_lbl],exp.labels[roi_lbl]) :
+
+        try:
+        
+            roi= sarpy.Scan(roi_scn).adata[roi_adata].data
+            scn = sarpy.Scan(scn)
+            
+            scn.store_adata(key=adata_save_lbl,data = roi,forceVal=forceVal) 
+
+            print('Successfully transferred roi from {0} to {1}'.format(roi.shortdirname, scn.shortdirname))
+        except:
+            print('FAILED to transfer roi from {0} to {1}'.format(scn, roi_scn))
+            pass
