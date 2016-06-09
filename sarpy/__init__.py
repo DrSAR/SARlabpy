1cb029c421b270b4deb16e01a09fd27135c74a66
Stefan A Reinsberg <stefan@phas.ubc.ca>
2016-06-09 13:03:39 -0700

    add import greeting to sarpy

--- sarpy/__init__.py
+++ sarpy/__init__.py
@@ -13,6 +13,12 @@ from .BRUKERIO.BRUKER_classes import (Scan, Study, StudyCollection,
 
 from .BRUKERIO.AData_classes import (adataroot, AData)
 
+print(('Imported sarpy v{0}\n'+
+      'git branch: {1} ({2}) -- {3}').format(
+      release,
+      repo_state['branch'],
+      'dirty' if repo_state['dirty'] else 'clean',
+      repo_state['sha1'][0:10]))
 #from inputoutput import (rois, write_csv, bulk_analyze, mriBoards)
 
 #from inputoutput.visu_pars_2_Nifti1Header import (visu_pars_2_Nifti1Header,
