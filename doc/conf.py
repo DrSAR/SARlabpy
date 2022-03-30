ec34809f254a904f692e13b30f7209760858e334
Stefan A Reinsberg <stefan@phas.ubc.ca>
2022-03-29 17:03:37 -0700

    minor doc updates

--- doc/conf.py
+++ doc/conf.py
@@ -26,7 +26,7 @@ sys.path.insert(0, os.path.abspath('..'))
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
-              'sphinx.ext.pngmath', 'sphinx.ext.linkcode']
+              'sphinx.ext.imgmath', 'sphinx.ext.linkcode']
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.pngmath', 'sphinx.ext.viewcode']
 #extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.pngmath']
 
--- sarpy/BRUKERIO/check_fid_acqp_method.py
+++ sarpy/BRUKERIO/check_fid_acqp_method.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-========================================
 Quick check for the presence of all sorts of BRUKER files in the 
 entire dataroot. A fully stocked scan should have
 acqp, method, fid, 2dseq. This results in a score of
 1 +   2 +     4 +  8 = 15. If one is missing, the score is less.
-========================================
 """
 import os
 import glob
